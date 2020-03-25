22050c01feb73ec9bf58df8659a4b20918b56827
dmytry-tkachuk <dmytry-tk@gmail.com>
2020-03-25 11:47:59 +0200

    Updates

---
+++ precache-manifest.a32de29c0a86e71cbad977ef9bcd8f13.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1b11ffb10dc029c6463c5438054de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44b2df6e96d7a950c1fe",
+    "url": "/static/css/main.4be3155a.chunk.css"
+  },
+  {
+    "revision": "5b1b50a6b02adda5148b",
+    "url": "/static/js/2.51e6b7ec.chunk.js"
+  },
+  {
+    "revision": "44b2df6e96d7a950c1fe",
+    "url": "/static/js/main.f95153e2.chunk.js"
+  },
+  {
+    "revision": "20a2045d1078201e812b",
+    "url": "/static/js/runtime-main.f1fa5b3e.js"
+  },
+  {
+    "revision": "946d38c8b0602beac3633cbbc500e759",
+    "url": "/static/media/1.946d38c8.jpg"
+  },
+  {
+    "revision": "b67f132404e34153bb3e3b9977fb33e7",
+    "url": "/static/media/accessories.b67f1324.png"
+  },
+  {
+    "revision": "9ed0e1c1342e7d8fa376647ebbdc4620",
+    "url": "/static/media/applewatch.9ed0e1c1.png"
+  },
+  {
+    "revision": "fe57e0f6fa111a173f71f924d0ddd941",
+    "url": "/static/media/imac.fe57e0f6.png"
+  },
+  {
+    "revision": "239e4c5eba49743b9c16abfd42527f0c",
+    "url": "/static/media/ipad.239e4c5e.png"
+  },
+  {
+    "revision": "7ac012296ab0e19481d06ce50fbe19a7",
+    "url": "/static/media/iphones.7ac01229.png"
+  },
+  {
+    "revision": "c1dd98f4d24416a4b2e14c15018492df",
+    "url": "/static/media/logo.c1dd98f4.png"
+  },
+  {
+    "revision": "5d5ac3a7ea60e97f025ba65cd188aafc",
+    "url": "/static/media/macbook.5d5ac3a7.png"
+  }
+]);
